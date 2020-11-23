69c33d126514e72da87054e6b3c177bdcfa93e28
Jeffry Steegmans <jeffrysteegmans@gmail.com>
2020-11-23 11:07:22 +0100

    Remove extra comma

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -169,7 +169,7 @@ $tweaks = @(
 	"InstallHyperV", # "UninstallHyperV",
 	"SetPhotoViewerAssociation", # "UnsetPhotoViewerAssociation",
 	"AddPhotoViewerOpenWith", # "RemovePhotoViewerOpenWith",
-	"InstallPDFPrinter"		# "UninstallPDFPrinter",
+	"InstallPDFPrinter",		# "UninstallPDFPrinter",
 	"UninstallXPSPrinter", # "InstallXPSPrinter",
 	"RemoveFaxPrinter", # "AddFaxPrinter",
 
@@ -183,7 +183,7 @@ $tweaks = @(
 
 	### Unpinning ###
 	"UnpinStartMenuTiles",
-	"UnpinTaskbarIcons",
+	"UnpinTaskbarIcons"
 
 	### Auxiliary Functions ###
 )
